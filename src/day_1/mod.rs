0a3f37d91442bb3b260ad012492a337bc367a659
Erik Tuerke <etuerke@googlemail.com>
2023-12-03 15:56:25 +0100

    day 1 part 2

--- src/day_1/mod.rs
+++ src/day_1/mod.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -26,4 +27,54 @@ pub fn solve_day_1_part_1() {
     }
 
     println!("day 1 part 1: {}", result);
+}
+
+pub fn solve_day_1_part_2() {
+    let digit_names = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+    let mut digit_map: HashMap<String, i32> = HashMap::new();
+    for (index, name) in digit_names.iter().enumerate() {
+        let number = (index as i32) + 1;
+        digit_map.insert(name.to_string(), number);
+        digit_map.insert(number.to_string(), number);
+    }
+
+    let file = File::open("inputs/day1.txt").unwrap();
+    let reader = BufReader::new(file);
+
+
+
+    let mut result = 0;
+
+    for line in reader.lines() {
+        if let Ok(line) = line {
+            let mut first_number = 0;
+            let mut last_number = 0;
+            let mut first_index = line.len() ;
+            let mut last_index = 0;
+            for (digit_name, number) in &digit_map {
+                if let Some(first) = line.find(digit_name) {
+                    if first < first_index {
+                        first_index = first;
+                        first_number = *number;
+                    }
+                };
+                if let Some(last) = line.rfind(digit_name) {
+                    if last >= last_index {
+                        last_index = last;
+                        last_number = *number;
+                    }
+                }
+            }
+
+            if first_number == 0 { first_number = last_number; }
+            if last_number == 0 { last_number = first_number; }
+
+            let number_in_line = format!("{}{}", first_number, last_number).parse::<i32>().unwrap();
+
+            result += number_in_line;
+        }
+    }
+
+    println!("day 1 part 2: {}", result);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ mod day_1;
 
 fn main() {
     day_1::solve_day_1_part_1();
+    day_1::solve_day_1_part_2();
 }
